7b52631f2c4764dca92bf08504608465d37010a6
pavel zagainov <pavel.zagainov@koedia.com>
2018-10-17 09:09:42 +0200

    format

--- pipeline.groovy
+++ pipeline.groovy
@@ -3,48 +3,47 @@ import hudson.*
 
 pipeline {
 	agent any
-    tools {
-        maven 'M3'
-    }
-    environment {
-        MANAGER = credentials('tomcat-manager')
-    }
-    stages {
+	tools {
+		maven 'M3'
+	}
+	environment {
+		MANAGER = credentials('tomcat-manager')
+	}
+	stages {
 		stage('SCM Checkout') {
-    	    steps {
-    		    checkout([$class: 'GitSCM', branches: [[name: '*/master']], userRemoteConfigs: [[credentialsId: 'zpavel', url: 'https://github.com/zpavel/myproject.git']]])
-    	    }
-    	}
-    	stage('Maven Build') {
-    	    steps {
-    		    sh "mvn clean package -T8"
-    	    }
-    	}
-    	stage('Tomcat Deploy') {
-    	    steps {
-    	        script {
-    	            pom = readMavenPom file: 'pom.xml'
+			steps {
+				checkout([$class: 'GitSCM', branches: [[name: '*/master']], userRemoteConfigs: [[credentialsId: 'zpavel', url: 'https://github.com/zpavel/myproject.git']]])
+			}
+		}
+		stage('Maven Build') {
+			steps {
+				sh "mvn clean package -T8"
+			}
+		}
+		stage('Tomcat Deploy') {
+			steps {
+				script {
+					pom = readMavenPom file: 'pom.xml'
 					sh "curl \"http://${MANAGER_USR}:${MANAGER_PSW}@myhost.com:8180/manager/text/undeploy?path=/\""
 					sh "curl --upload-file mywebapp/target/mywebapp-${pom.version}.war \"http://${MANAGER_USR}:${MANAGER_PSW}@myhost.com:8180/manager/text/deploy?path=/&war=mywebapp\""
-            			
-            			        }
-    		}
-    	}
-    	stage('Results') {
+				}
+			}
+		}
+		stage('Results') {
 			steps {
-    		    archiveArtifacts artifacts: '**/target/*.jar'
-    		    junit '**/target/surefire-reports/TEST-*.xml'
-    	     }
-    	}
-    }
-    post {
-        success {
-            slackSend (color: '#00FF00', message: getChangeSet())
-        }
-        failure {
-            slackSend (color: '#cc0000', message: "FAILED: Job '${env.JOB_NAME} [${env.BUILD_NUMBER}]' (${env.BUILD_URL})")
-        }
-    }
+				archiveArtifacts artifacts: '**/target/*.jar'
+				junit '**/target/surefire-reports/TEST-*.xml'
+			}
+		}
+	}
+	post {
+		success {
+			slackSend (color: '#00FF00', message: getChangeSet())
+		}
+		failure {
+			slackSend (color: '#cc0000', message: "FAILED: Job '${env.JOB_NAME} [${env.BUILD_NUMBER}]' (${env.BUILD_URL})")
+		}
+	}
 }
 
 @NonCPS
